0699855a320838aa5f05eb58be8e66757580b983
jbeauno <morinemm@yahoo.fr>
2016-05-18 23:12:41 +0200

    OnClick sound

--- Assets/Scripts/GameAudio.cs
+++ Assets/Scripts/GameAudio.cs
@@ -16,34 +16,9 @@ public class GameAudio : MonoBehaviour {
 	}
     
     // Update is called once per frame
-    void Update () {
-
-        //part 1: Mix Music = f(camera.altitude)
-        /*
-		if (Camera) {
-			//switch (Camera.altitude)
-			//Camera.altitude
-			if (Camera.altitude <= 1)
-				mixMusic("drumbass");
-			else if (Camera.altitude <= 8)
-				mixMusic("drumbassfx");
-			else
-				mixMusic("drumbells", 0);
-		}
-		else
-			mixMusic("drumbassfx");
-
-
-		GameObject.Find ("DebugLine6").GetComponent <Text> ().text = Camera.altitude.ToString();
-		GameObject.Find ("DebugLine7").GetComponent <Text>().text = ActiveSnapshot.name;
-        */
-
+    void Update () {    
 		mixMusic();
-
-		GameObject.Find ("DebugLine6").GetComponent <Text> ().text = Camera.altitude.ToString();
-		//GameObject.Find ("DebugLine7").GetComponent <Text>().text = ActiveSnapshot.name;
-
-        //todo: part 2: play fx .....        
+        applyFx();
 	}
 	    
     private void mixMusic(string SnapshotName, float TransitionTime=2)
@@ -56,13 +31,31 @@ public class GameAudio : MonoBehaviour {
 		}
 	}
 
+    private void applyFx()
+    {
+        if (Input.GetMouseButtonDown(0))
+            GameObject.Find("onclick").GetComponent<AudioSource>().Play(0);
+    }
+
     private void mixMusic()
-    {     
-		//each track volume = f(AudioTrackBehaviour)
+    {
+        //each track volume = f(AudioTrackBehaviour)
+        GameObject.Find("DebugLine6").GetComponent<Text>().text = Camera.altitude.ToString();
+        //Track1: busy
         MixerMain.SetFloat("bells_volume", GameObject.Find("bells").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
         MixerMain.SetFloat("bass_fx_volume", GameObject.Find("bassfx").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
         MixerMain.SetFloat("drum_trad_volume", GameObject.Find("drumtrad").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
         MixerMain.SetFloat("bass_massive_volume", GameObject.Find("bass-massive").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
         MixerMain.SetFloat("bass_melody_volume", GameObject.Find("bass-melody").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+
+        //Track2: Planos
+        MixerMain.SetFloat("brass_volume", GameObject.Find("brass").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("drums_volume", GameObject.Find("drums").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("heartbeat_volume", GameObject.Find("heartbeat").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("lonely_molecule_volume", GameObject.Find("lonely_molecule").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("napelec_volume", GameObject.Find("napelec").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("planos_bass_volume", GameObject.Find("planos_bass").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+        MixerMain.SetFloat("planos_bells_volume", GameObject.Find("planos_bells").GetComponent<AudioTrackBehaviour>().GetVolume(Camera.altitude));
+
     }
 }
